06920d3a68d15b66e333efe6bf4d36ddfe18e740
Kenzo <hohochan66@gmail.com>
2023-10-05 23:01:21 +0800

    header component

---
+++ app/components/header.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="p-4 bg-white shadow-md">
+      <nav className="container mx-auto flex justify-between">
+        <Link href="/">
+          <span className="text-2xl font-bold text-blue-500 cursor-pointer hidden lg:inline">
+            () =&gt; Page.
+          </span>
+          <span className="text-2xl font-bold text-yellow-500 cursor-pointer">
+            home
+          </span>
+          <span className="text-2xl font-bold text-blue-500 cursor-pointer">
+            ();
+          </span>
+        </Link>
+        <div className="space-x-4">
+          <span className="hidden lg:inline">const navBar = </span>
+          <span>[</span>
+          <Link href="#about">
+            <span className="text-blue-500 cursor-pointer">About</span>
+          </Link>
+          <span>,</span>
+          <Link href="#projects">
+            <span className="text-blue-500 cursor-pointer">Projects</span>
+          </Link>
+          <span>,</span>
+          <Link href="#education">
+            <span className="text-blue-500 cursor-pointer">Education</span>
+          </Link>
+          <span>,</span>
+          <Link href="#links">
+            <span className="text-blue-500 cursor-pointer">Links</span>
+          </Link>
+          <span>,</span>
+          <Link href="#contact">
+            <span className="text-blue-500 cursor-pointer">Contact</span>
+          </Link>
+          <span>]</span>
+        </div>
+      </nav>
+    </header>
+  );
+}
